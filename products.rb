345b1ceae42e3b7752bafb1e59ab5c7f13bd7f88
Abimathic <abimathic14@gmail.com>
2025-07-18 15:57:38 +0100

    Add product catalog with widget codes and prices

---
+++ products.rb
@@ -0,0 +1,6 @@
+# product_catalog_details
+PRODUCTS = {
+  "R01" => { name: "Red Widget", price: 32.95 },
+  "G01" => { name: "Green Widget", price: 24.95 },
+  "B01" => { name: "Blue Widget", price: 7.95 }
+}
